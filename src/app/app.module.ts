bec2173ea8e982901830c4251c9c00551c1d7976
shilko2013 <shilko2013@mail.ru>
2018-12-05 12:07:55 +0300

    login-field component added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginFieldComponent } from './login-field/login-field.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginFieldComponent
   ],
   imports: [
     BrowserModule,
