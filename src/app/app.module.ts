a102087aa21cc4dd53389bf48e0ca43f2dccd296
shilko2013 <shilko2013@mail.ru>
2018-12-07 08:58:23 +0300

    http queries added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginFieldComponent } from './login-field/login-field.component';
+import {UserLoginService} from './user-login/user-login.service';
 
 @NgModule({
   declarations: [
@@ -12,9 +14,10 @@ import { LoginFieldComponent } from './login-field/login-field.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [UserLoginService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
